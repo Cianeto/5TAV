fd25bc9694ae4f365c327cd1d05b02d6a75d77e8
Cianeto <gbdopu02@gmail.com>
2024-10-20 00:05:39 -0300

    more progress, making the perfect player now

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import choice
 
 ALEATORIO = 0
 PERFEITO = 1
@@ -11,6 +11,11 @@ class Player:
         self.player_symbol = player_symbol
         self.board = board
 
+    def check_empty(self, position):
+        if self.board[position] != 0:
+            return 1
+        return 0
+
     def move(self):
         self.board[0] += 1
         play = {
@@ -18,13 +23,35 @@ class Player:
             1: self.perfect_move,
         }
         play[self.player_type](self.player_symbol)
+        # Game().print_board(self.board)
 
     def random_move(self, p):
-        """ """
+        empty_positions = [i for i in range(1, 10) if self.board[i] == 0]
+        if empty_positions:
+            position = choice(empty_positions)
+            self.board[position] = p
 
     def perfect_move(self, p):
-        """ """
-
+        opp = -1 if p == 1 else 1
+        b = self.board
+        if b[0] == 1:
+            b[1] = p
+        elif b[0] == 2:
+            """  """
+        elif b[0] == 3:
+            """  """
+        elif b[0] == 4:
+            """  """
+        elif b[0] == 5:
+            """  """
+        elif b[0] == 6:
+            """  """
+        elif b[0] == 7:
+            """  """
+        elif b[0] == 8:
+            """  """
+        elif b[0] == 9:
+            """  """
 
 class Game:
     def __init__(self):
@@ -33,7 +60,7 @@ class Game:
             4 | 5 | 6
             7 | 8 | 9 """
         self.p1 = Player(ALEATORIO, 1, self.board)
-        self.p2 = Player(ALEATORIO, -1, self.board)
+        self.p2 = Player(PERFEITO, -1, self.board)
         self.p1_wins = 0
         self.p2_wins = 0
         self.draws = 0
@@ -51,37 +78,82 @@ class Game:
             [3, 5, 7],
         ]
 
+    def print_board(self, b):
+        formatted_board = f"""
+        {b[1]} | {b[2]} | {b[3]}
+        -----------
+        {b[4]} | {b[5]} | {b[6]}
+        -----------
+        {b[7]} | {b[8]} | {b[9]}
+        """
+        print(formatted_board)
+
     def reset_board(self):
-        self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.board.clear()
+        self.board.extend([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def check_win(self):
         for combo in self.win_combinations:
             total = self.board[combo[0]] + self.board[combo[1]] + self.board[combo[2]]
             if total == 3:
                 self.p1_wins += 1
+                # self.print_board(self.board)
+                # print("WINNER: P1\n")
                 return 1
             elif total == -3:
                 self.p2_wins += 1
+                # self.print_board(self.board)
+                # print("WINNER: P2\n")
                 return 1
         return 0
 
     def run(self):
-        for i in range(100):
+        for i in range(5000):
+
+            # SEM ALTERNÂNCIA
             for j in range(5):
                 self.p1.move()
-                if self.board[0] >= 5:
-                    if self.check_win():
-                        break
+                if self.board[0] >= 5 and self.check_win():
+                    break
 
-                if j == 5:
+                if j == 4:
                     self.draws += 1
+                    # self.print_board(self.board)
+                    # print("DRAW\n")
                     break
 
                 self.p2.move()
-                if self.board[0] >= 5:
-                    if self.check_win():
+                if self.board[0] >= 5 and self.check_win():
+                    break
+
+            # COM ALTERNÂNCIA
+            """ starting_player = i % 2
+            for j in range(5):
+                if starting_player == 0:
+                    self.p1.move()
+                    if self.board[0] >= 5 and self.check_win():
                         break
-                    
+                    if j == 4:
+                        self.draws += 1
+                        # self.print_board(self.board)
+                        # print("DRAW\n")
+                        break
+                    self.p2.move()
+                    if self.board[0] >= 5 and self.check_win():
+                        break
+                else:
+                    self.p2.move()
+                    if self.board[0] >= 5 and self.check_win():
+                        break
+                    if j == 4:
+                        self.draws += 1
+                        # self.print_board(self.board)
+                        # print("DRAW\n")
+                        break
+                    self.p1.move()
+                    if self.board[0] >= 5 and self.check_win():
+                        break """
+
             self.reset_board()
         print(f"P1 -> {self.p1_wins}\nP2 -> {self.p2_wins}\nEMPATES -> {self.draws}")
 
